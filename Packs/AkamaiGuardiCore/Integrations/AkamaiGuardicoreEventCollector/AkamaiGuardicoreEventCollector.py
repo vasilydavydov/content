3de708803944e5f7b0f6c136362498ce80f80439
sharonfi99 <sfish@paloaltonetworks.com>
2023-12-26 11:29:14 +0200

    changes to akamai guradicore

---
+++ Packs/AkamaiGuardiCore/Integrations/AkamaiGuardicoreEventCollector/AkamaiGuardicoreEventCollector.py
@@ -0,0 +1,252 @@
+import demistomock as demisto
+from CommonServerPython import *
+from AkamaiGuardicoreApiModule import *
+
+
+""" CONSTANTS """
+
+MAX_FETCH = 1000
+VENDOR = "akamai"
+PRODUCT = "guardicore"
+
+
+""" CLIENT CLASS """
+
+
+class Client(AkamaiGuardicoreClient):
+    """
+    Client for Akamai Guardicore Event Collector
+    """
+
+    def get_events(self, start_time, end_time, limit, offset) -> dict[str, Any]:
+        """
+        Get events from Guardicore API using the incidents endpoint.
+        """
+        params = {
+            "from_time": start_time,
+            "to_time": end_time,
+            "limit": limit,
+            "offset": offset,
+            "sort": "start_time",
+        }
+        return self._http_request(method="GET", url_suffix="/incidents", params=params)
+
+
+""" HELPER FUNCTIONS """
+
+
+def add_time_to_events(events: list[dict]) -> None:
+    """Adds the _time key to the events.
+
+    Args:
+        events: list[dict] - list of events to add the _time key to.
+
+    Returns:
+        None
+    """
+    if events:
+        [
+            event.update({"_time": timestamp_to_datestring(event["start_time"])})
+            for event in events
+            if "start_time" in event
+        ]
+
+
+def delete_id_key_from_events(events: list[dict]) -> None:
+    """Deletes the _id key from the events.
+
+    Args:
+        events: list[dict] - list of events to deletes the _id key.
+
+    Returns:
+        None
+    """
+    [event.pop("_id", None) for event in events]
+
+
+def handle_events_labels(events: list[dict]) -> None:
+    """Add the labels to the "destination asset" and "source asset".
+
+    Args:
+        events: list[dict] - list of events to handle the labels.
+
+    Returns:
+        None
+    """
+    for event in events:
+        destination_asset: dict = event["destination_asset"]
+        destination_asset_labels = destination_asset["labels"] = {}
+
+        source_asset: dict = event["source_asset"]
+        source_asset_labels = source_asset["labels"] = {}
+
+        labels: list[dict] = event.pop("labels", [])
+        for label in labels:
+            for asset_id in label.get("asset_ids", []):
+                if asset_id == destination_asset.get("vm_id"):
+                    destination_asset_labels[label["key"]] = label["value"]
+
+                if asset_id == source_asset.get("vm_id"):
+                    source_asset_labels[label["key"]] = label["value"]
+
+
+def format_events(events: list[dict]) -> None:
+    delete_id_key_from_events(events)
+    handle_events_labels(events)
+
+
+def create_last_run(events: list[dict], end_time: int) -> dict:
+    if events:
+        start_time_last_event = events[-1]["start_time"]
+        ids_in_start_time_last_event = [
+            event["id"]
+            for event in events
+            if event["start_time"] // 1000 == start_time_last_event // 1000
+        ]
+        return {
+            "from_ts": start_time_last_event,
+            "ids_in_start_time_last_event": ids_in_start_time_last_event,
+        }
+    else:
+        return {"from_ts": end_time}
+
+
+""" COMMAND FUNCTIONS """
+
+
+def test_module(client: Client) -> str:
+    """
+    Tests API connectivity and authentication'
+    When 'ok' is returned it indicates the integration works like it is supposed to and connection to the service is
+    successful.
+    Raises exceptions if something goes wrong.
+    Args:
+        client (Client): Gurdicore client to use.
+        params (Dict): Integration parameters.
+    Returns:
+        str: 'ok' if test passed, anything else will raise an exception and will fail the test.
+    """
+
+    try:
+        client.login()
+
+    except Exception as e:
+        if "UNAUTHORIZED" in str(e):
+            return "Authorization Error: make sure the username and password are correctly set"
+        else:
+            raise e
+
+    return "ok"
+
+
+def get_events(client: Client, args: dict):
+    """
+    Gets events from Guardicore API.
+    """
+    start_time = date_to_timestamp(
+        arg_to_datetime(args.get("from_date", "1 minute ago"))
+    )
+    end_time = (
+        date_to_timestamp(arg_to_datetime(args.get("to_time")))
+        if "to_time" in args
+        else int(datetime.now().timestamp() * 1000)
+    )
+    limit = arg_to_number(args.get("limit")) or MAX_FETCH
+    limit = min(limit, MAX_FETCH)
+    offset = int(args.get("offset", 0))
+    response = client.get_events(start_time, end_time, limit, offset)
+    events = response["objects"]
+    format_events(events)
+    hr = tableToMarkdown(name=f"Found {len(events)} events", t=events)
+    return events, CommandResults(readable_output=hr, raw_response=events)
+
+
+def fetch_events(
+    client: Client, params: dict, last_run: dict
+) -> tuple[List[dict], dict]:
+    """
+    Fetches events from Guardicore API.
+    """
+    start_time = date_to_timestamp(
+        arg_to_datetime(last_run.get("from_ts", "1 minute ago"))
+    )
+    end_time = int(datetime.now().timestamp() * 1000)
+    demisto.debug(
+        f"Getting events from: {timestamp_to_datestring(start_time)}, till: {timestamp_to_datestring(end_time)}"
+    )
+    offset = arg_to_number(last_run.get("offset")) or 0
+    limit = arg_to_number(params.get("max_events_per_fetch")) or MAX_FETCH
+    limit = min(limit, MAX_FETCH)
+
+    response = client.get_events(start_time, end_time, limit, offset)
+    events: list = response["objects"]
+    format_events(events)
+    if ids_in_start_time_last_event := last_run.get("ids_in_start_time_last_event"):
+        events = [
+            event for event in events if event["id"] not in ids_in_start_time_last_event
+        ]
+    demisto.debug(f"Fetched {len(events)} events.")
+
+    last_run = create_last_run(events, end_time)
+
+    return events, last_run
+
+
+""" MAIN FUNCTION """
+
+
+def main() -> None:  # pragma: no cover
+    """
+    main function, parses params and runs command functions
+    """
+
+    params = demisto.params()
+    args = demisto.args()
+    command = demisto.command()
+    username = params.get("credentials", {}).get("identifier")
+    password = params.get("credentials", {}).get("password")
+    base_url = urljoin(params.get("url"), "/api/v3.0")
+    verify_certificate = not params.get("insecure", False)
+    proxy = params.get("proxy", False)
+
+    demisto.debug(f"Command being called is {command}")
+    try:
+        client = Client(
+            username=username,
+            password=password,
+            base_url=base_url,
+            proxy=proxy,
+            verify=verify_certificate,
+        )
+        if command == "test-module":
+            # This is the call made when pressing the integration Test button.
+            result = test_module(client)
+            return_results(result)
+
+        client.login()
+        if command == f"{PRODUCT}-get-events":
+            should_push_events = argToBoolean(args.get("should_push_events", False))
+            events, results = get_events(client, args)
+            return_results(results)
+            if should_push_events:
+                add_time_to_events(events)
+                send_events_to_xsiam(events, vendor=VENDOR, product=PRODUCT)
+
+        elif command == "fetch-events":
+            last_run = demisto.getLastRun() or {}
+            events, new_last_run = fetch_events(client, params, last_run)
+            add_time_to_events(events)
+
+            send_events_to_xsiam(events, vendor=VENDOR, product=PRODUCT)
+            demisto.debug(f"Set new last run with: {new_last_run}")
+            demisto.setLastRun(new_last_run)
+
+    # Log exceptions and return errors
+    except Exception as e:
+        return_error(f"Failed to execute {command} command.\nError:\n{str(e)}")
+
+
+""" ENTRY POINT """
+
+if __name__ in ("__main__", "__builtin__", "builtins"):
+    main()
---
+++ Packs/AkamaiGuardiCore/Integrations/AkamaiGuardicoreEventCollector/AkamaiGuardicoreEventCollector_test.py
@@ -0,0 +1,252 @@
+import pytest
+from pytest_mock import MockerFixture
+
+
+def test_test_module_success(mocker: MockerFixture):
+    """
+    Given:
+        Client with valid credentials
+    When:
+        test_module is called
+    Then:
+        It should return 'ok'
+    """
+    from AkamaiGuardicoreEventCollector import test_module
+
+    client = mocker.Mock()
+    client.login.return_value = None
+    assert test_module(client) == "ok"
+
+
+def test_test_module_failure(mocker: MockerFixture):
+    """
+    Given:
+        Client with invalid credentials
+    When:
+        test_module is called
+    Then:
+        It should raise an exception
+    """
+    from AkamaiGuardicoreEventCollector import test_module
+
+    client = mocker.Mock()
+    client.login.side_effect = Exception("Something")
+
+    with pytest.raises(Exception):
+        test_module(client)
+
+
+def test_test_module_failure_Unauthorized(mocker: MockerFixture):
+    """
+    Given:
+        Client with invalid credentials
+    When:
+        test_module is called
+    Then:
+        It should return an error message
+    """
+    from AkamaiGuardicoreEventCollector import test_module
+
+    client = mocker.Mock()
+    client.login.side_effect = Exception("UNAUTHORIZED")
+
+    result = test_module(client)
+    assert (
+        result
+        == "Authorization Error: make sure the username and password are correctly set"
+    )
+
+
+def test_add_time_to_events(mocker: MockerFixture):
+    """
+    Given:
+        A list of events
+    When:
+        Calling add_time_to_events
+    Then:
+        Added the _time key from the start_time field to the event
+    """
+    from AkamaiGuardicoreEventCollector import add_time_to_events
+
+    events = [{"id": 1, "start_time": 100}, {"id": 2, "start_time": 100}]
+    expected = [
+        {"id": 1, "start_time": 100, "_time": "1970-01-01T00:01:40Z"},
+        {"id": 2, "start_time": 100, "_time": "1970-01-01T00:01:40Z"},
+    ]
+
+    mocker.patch(
+        "AkamaiGuardicoreEventCollector.timestamp_to_datestring",
+        return_value="1970-01-01T00:01:40Z",
+    )
+    add_time_to_events(events)
+
+    assert events == expected
+
+
+def test_delete_id_key_from_events():
+    """
+    Given:
+        A list of events
+    When:
+        Calling delete_id_key_from_events
+    Then:
+        The "_id" field should be removed from each event
+    """
+    from AkamaiGuardicoreEventCollector import delete_id_key_from_events
+
+    events = [{"_id": "1", "id": "1"}]
+
+    delete_id_key_from_events(events)
+
+    assert "_id" not in events[0]
+
+
+def test_handle_events_labels():
+    """
+    Given:
+        A list of events with labels
+    When:
+        Calling handle_events_labels
+    Then:
+        The labels should be added to the source and destination assets
+    """
+    from AkamaiGuardicoreEventCollector import handle_events_labels
+
+    events = [
+        {
+            "id": "1",
+            "destination_asset": {"vm_id": "1"},
+            "source_asset": {"vm_id": "2"},
+            "labels": [
+                {"asset_ids": ["1"], "key": "key1", "value": "value1"},
+                {"asset_ids": ["2"], "key": "key2", "value": "value2"},
+                {"asset_ids": ["1", "2"], "key": "key3", "value": "value3"},
+            ],
+        }
+    ]
+
+    handle_events_labels(events)
+
+    assert events[0]["destination_asset"]["labels"] == {
+        "key1": "value1",
+        "key3": "value3",
+    }
+    assert events[0]["source_asset"]["labels"] == {"key2": "value2", "key3": "value3"}
+
+
+def test_create_last_run():
+    """
+    Given:
+        A list with events, some with the same start time (if the milliseconds is ignored)
+    When:
+        create_last_run is called
+    Then:
+        It should return a dict with the last event's start time and all ids that match
+    """
+    from AkamaiGuardicoreEventCollector import create_last_run
+
+    events = [
+        {"id": "1", "start_time": 1222222222555},
+        {"id": "2", "start_time": 1222222223555},
+        {"id": "3", "start_time": 1222222223755},
+    ]
+    result = create_last_run(events, 0)
+    assert result == {
+        "from_ts": 1222222223755,
+        "ids_in_start_time_last_event": ["2", "3"],
+    }
+
+
+def test_get_events_success(mocker: MockerFixture):
+    """
+    Given:
+        - Client instance
+        - Valid args
+    When:
+        - Calling get_events
+    Then:
+        - Should return events and CommandResults
+    """
+    from AkamaiGuardicoreEventCollector import get_events
+
+    client = mocker.Mock()
+    mocker.patch(
+        "AkamaiGuardicoreEventCollector.format_events",
+        return_value=None,
+    )
+    mock_events = [{"id": 1}]
+    client.get_events.return_value = {"objects": mock_events}
+
+    args = {"from_date": "1 minute ago"}
+
+    events, results = get_events(client, args)
+
+    assert events == mock_events
+    assert results.readable_output
+    assert results.raw_response == mock_events
+
+
+def test_fetch_events_full_fetch(mocker: MockerFixture):
+    """
+    Given:
+        - Client
+        - Last run with no IDs
+        - Params with high limit
+    When:
+        fetch_events is called
+    Then:
+        It should return all events from API
+    """
+    from AkamaiGuardicoreEventCollector import fetch_events
+
+    mocker.patch(
+        "AkamaiGuardicoreEventCollector.format_events",
+        return_value=None,
+    )
+    mocker.patch(
+        "AkamaiGuardicoreEventCollector.create_last_run",
+        return_value={},
+    )
+
+    client = mocker.Mock()
+    client.get_events.return_value = {"objects": [{"id": 1}, {"id": 2}]}
+
+    last_run = {}
+    params = {"max_events_per_fetch": 1000}
+
+    events, _ = fetch_events(client, params, last_run)
+
+    assert len(events) == 2
+
+
+def test_fetch_events_partial_fetch(mocker: MockerFixture):
+    """
+    Given:
+        - Client
+        - Last run with some IDs
+        - Params with low limit
+    When:
+        fetch_events is called
+    Then:
+        It should only return new events, without duplicated IDs
+    """
+    from AkamaiGuardicoreEventCollector import fetch_events
+
+    mocker.patch(
+        "AkamaiGuardicoreEventCollector.format_events",
+        return_value=None,
+    )
+    mocker.patch(
+        "AkamaiGuardicoreEventCollector.create_last_run",
+        return_value={},
+    )
+    client = mocker.Mock()
+    client.get_events.return_value = {"objects": [{"id": 1}, {"id": 2}, {"id": 3}]}
+
+    last_run = {"ids_in_start_time_last_event": [3]}
+    params = {"max_events_per_fetch": 2}
+
+    events, _ = fetch_events(client, params, last_run)
+
+    assert len(events) == 2
+    assert not any(event["id"] == 3 for event in events)
